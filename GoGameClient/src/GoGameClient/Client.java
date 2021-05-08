26234df5a298be224acc26131c862da429d6a95f
melikekara <43819337+melikekara@users.noreply.github.com>
2021-05-08 03:38:37 +0300

    Update Client.java

--- GoGameClient/src/GoGameClient/Client.java
+++ GoGameClient/src/GoGameClient/Client.java
@@ -5,7 +5,7 @@
  */
 package GoGameClient;
 
-
+import Game.FirstPage;
 import Game.Message;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -15,6 +15,18 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import Game.Game1;
 import static GoGameClient.Client.sInput;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 /**
  *
@@ -23,16 +35,23 @@ import static GoGameClient.Client.sInput;
 // serverdan gelecek mesajları dinleyen thread
 class Listen extends Thread {
 
+    static int x1 ;
+    static int y1 ;
+
     public void run() {
+
         //soket bağlı olduğu sürece dön
         while (Client.socket.isConnected()) {
+            
             try {
-                //mesaj gelmesini bloking olarak dinyelen komut
+             
                 Message received = (Message) (sInput.readObject());
-                //mesaj gelirse bu satıra geçer
-                //mesaj tipine göre yapılacak işlemi ayır.
+               
                 switch (received.type) {
                     case Name:
+                        break;
+                    case Loc:
+                        Game1.game1.rivalSelection = (int) received.content;
                         break;
                     case RivalConnected:
                         String name = received.content.toString();
@@ -45,29 +64,27 @@ class Listen extends Thread {
                     case Text:
                         Game1.game1.txt_area2.setText(received.content.toString());
                         break;
-                    case Selected:
-                        Game1.game1.rivalSelection = (int) received.content;
-
-                        break;
-
                     case Bitis:
                         break;
-
+                        
                 }
-
+                
             } catch (IOException ex) {
-
+                
                 Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-                //Client.Stop();
+               
                 break;
             } catch (ClassNotFoundException ex) {
                 Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-                //Client.Stop();
+            
                 break;
             }
         }
 
     }
+
+   
+
 }
 
 public class Client {
@@ -93,7 +110,7 @@ public class Client {
             Client.sOutput = new ObjectOutputStream(Client.socket.getOutputStream());
             Client.listenMe = new Listen();
             Client.listenMe.start();
-            
+
             //ilk mesaj olarak isim gönderiyorum
             Message msg = new Message(Message.Message_Type.Name);
             msg.content = Game1.game1.lbl_name.getText();
