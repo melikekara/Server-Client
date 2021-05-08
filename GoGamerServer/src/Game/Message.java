8b0f63bacfc674b7b0b2e3b51425fd907da1d175
melikekara <43819337+melikekara@users.noreply.github.com>
2021-05-08 03:51:26 +0300

    Update Message.java

--- GoGamerServer/src/Game/Message.java
+++ GoGamerServer/src/Game/Message.java
@@ -11,11 +11,8 @@ package Game;
  */
 
 public class Message implements java.io.Serializable {
-    //mesaj tipleri enum 
-    public static enum Message_Type {None, Name, Disconnect,RivalConnected, Text, Selected, Bitis,Start,}
-    //mesajın tipi
+    public static enum Message_Type {None, Name, Disconnect,RivalConnected, Text,Loc, Bitis,Start,}
     public Message_Type type;
-    //mesajın içeriği obje tipinde ki istenilen tip içerik yüklenebilsin
     public Object content;
     public Message(Message_Type t)
     {
